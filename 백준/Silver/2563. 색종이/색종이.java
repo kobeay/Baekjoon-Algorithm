26e5ecf3c5ea7f8a64ccfb66961e289d20dd850c
kobeay <rhaudwns4981@naver.com>
2025-07-13 23:29:39 +0900

    [Silver V] Title: 색종이, Time: 204 ms, Memory: 17796 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        boolean[][] arr = new boolean[100][100];
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+
+            for (int j = x; j < x + 10; j++) {
+                for (int k = y; k < y + 10; k++) {
+                    arr[j][k] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (arr[i][j]) {
+                    count++;
+                }
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
